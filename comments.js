2b9019283e843f122fa5b14aba57e754bedfd5e8
Wojciech Bojarczuk <bojarczuk@protonmail.com>
2018-08-12 11:39:55 +0200

    Modification comments.js

--- comments.js
+++ comments.js
@@ -5,8 +5,18 @@ function comments(state = [], action) {
         case ADD_COMMENT:
             return [{
                 id: action.id,
-                text: action.text
+                text: action.text,
                 votes: 0
+            }
+                , ...state.comments];
+
+        case REMOVE_COMMENT:
+            return state.filter(comment =>comment.id !== action.id);
+
+        case EDIT_COMMENT:
+            return [{
+                id: action.id,
+                text: action.text
             }
                 , ...state.comments];
     }
